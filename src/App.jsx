940f8860097559e0c819cea34f2cbbd01b1a673f
Your Name <you@example.com>
2026-02-22 01:21:40 +0300

    changing timer work

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import { useEffect} from "react";
+import {  useState } from "react";
 import "./components/TextBox/text-box.style.scss";
 import Timer from "./components/timer/timer.component";
 
 const App = () => {
+  const [text, setText] = useState("");
+  const [startTimer, setStartTimer] = useState(false);
 
-      const KeypressEvent = (e) =>{
-        return(
-            alert("you clicked :" + "  " + e.key)
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setText(value);
 
-        )
-      }
-        useEffect(()=> {
-        document.addEventListener('keypress', KeypressEvent)
-        },[])
-  return (
-    <div>
-      <input type="text" className="form__input" id="name" placeholder="click here a letters" /> 
-      <Timer ></Timer> 
-    </div>
+    if (!startTimer && value.length > 0) {
+      setStartTimer(true);
+    }
+  };
+
+    return (
+      <div>
+        <input type="text" className="form__input" id="name" placeholder="click here a letters"   value={text}
+        onChange={handleChange} /> 
+        <Timer start={startTimer} ></Timer> 
+      </div>
       
-  );
+
+
+
+      
+    );
 }
 
 export default App;
--- src/components/timer/timer.component.jsx
+++ src/components/timer/timer.component.jsx
@@ -1,36 +1,24 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import './timer.style.scss'
 
-const Timer = () => {
+const Timer = ({start}) => {
     const INITIAL_TIME = 60;
     const [time, setTime] = useState(INITIAL_TIME);
-    const intervalRef = useRef(null);
-
-    const startTimer = () => {
-    clearInterval(intervalRef.current);
-    setTime(INITIAL_TIME);
-
-    intervalRef.current = setInterval(() => {
-        setTime((prev) => {
-        if (prev <= 1) {
-            clearInterval(intervalRef.current);
-            intervalRef.current = null;
-            return 0;
-        }
-        return prev - 1;
-        });
-    }, 1000);
-    };
-
     
-    useEffect(() => {
-    return () => clearInterval(intervalRef.current);
-    }, []);
+    useEffect(() =>{
+        if (!start) return;
+
+        const interval = setInterval(() => {
+            setTime((prev) => (prev > 0 ? prev - 1 : 0));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [start]);
 
     return (
     <div className="timer">
         <h1>{time} sec</h1>
-        <button onClick={startTimer}>Start</button>
+        {/* <button onClick={startTimer} >Start</button> */}
     </div>
     );
 };
